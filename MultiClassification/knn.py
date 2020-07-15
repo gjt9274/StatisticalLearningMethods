00fba74286d2fb8e9d5de92708914b8e6b31a4ac
gjt9274 <gjt9274@gmail.com>
2020-07-15 21:58:08 +0800

    knn

--- MultiClassification/knn.py
+++ MultiClassification/knn.py
@@ -9,8 +9,8 @@
 
 import numpy as np
 from load_data import load_data
-from math import sqrt
-from collections import Counter,namedtuple
+from collections import Counter
+from matplotlib import pyplot as plt
 import time
 
 
@@ -71,6 +71,9 @@ class KdNode(object):
         self.left = left
         self.right = right
 
+    def get_attr(self):
+        return self.dom_elt,self.split,self.left,self.right
+
 class KdTree(object):
     def __init__(self,data):
         self.k = len(data[0]) #特征维度
@@ -86,11 +89,12 @@ class KdTree(object):
         Returns:
 
         """
-        if not data_set: #递归边界，数据集为空返回None
+        if not len(data_set): #递归边界，数据集为空返回None
             return None
 
         # 按要进行分割的那一维特征进行排序
-        data_set.sort(key = lambda x:x[split])
+        # data_set.sort(key = lambda x:x[split])
+        sorted(data_set,key=lambda x:x[split])
         split_pos = len(data_set) // 2
         median = data_set[split_pos] #中位数分割点
         split_next = (split+1) % self.k #更新维度
@@ -103,82 +107,124 @@ class KdTree(object):
             self._build_tree(split_next,data_set[split_pos+1:]) #右子树
         )
 
-#定义一个namedtuple，用来存储最近坐标点，最近距离，访问过的节点数
-result = namedtuple("Result_tuple",
-                    "nearest_point nearest_dist nodes_visited")
+class KnnBasedOnKdTree(object):
+    def __init__(self,data,label,p=2):
+        self.x = data
+        self.y = label
+        self.p = p
+        self.kd_tree = KdTree(data)
+        # 定义一个namedtuple，用来存储最近坐标点，最近距离，标签
+        self.nearest = []
 
-def find_nearest(tree,point):
-    """
-    找到样本点point的最近临近点
-    Args:
-        tree (KdTree): kd树
-        point (array): 样本点 (1,k)
+    def _search_leaf(self,stack,tree,target):
+        travel_tree = tree
+        while travel_tree:
+            dom_elt,axes,lchile,rchild = travel_tree.get_attr()
+            if target[axes] >= dom_elt[axes]:
+                next_node = rchild
+                next_direction = 'right' #记录哪个方向被访问过了
+            else:
+                next_node = lchile
+                next_direction = 'left'
 
-    Returns:
+            stack.append((travel_tree,next_direction))
+            travel_tree = next_node
 
-    """
-    k = len(point) #样本维度
+    def _get_farthest(self):
+        """获取k个点中最远的元素"""
+        farthest = None
+        for item  in self.nearest:
+            if not farthest:
+                farthest = item
+            elif farthest[1] < item[1]:
+                farthest = item
+        return farthest
 
-    def travel(kd_node,target,max_dist):
-        if kd_node is None:
-            return result([0]*k, float("inf"),0)
 
-        nodes_visited = 1
+    def _check_nearest(self,current,target,k):
+        d = np.linalg.norm(current-target,ord=self.p)
+        l = len(self.nearest)
 
-        s = kd_node.split # 获取分割的维度
-        pivot = kd_node.dom_elt # 进行分割的轴
-
-        if target[s] <= pivot[s]: #如果目标点第 s 维小于分割点的对应维度值，说明目标点离左子树更近
-            nearer_node = kd_node.left
-            further_node = kd_node.right
+        if l < k: #说明还没有满k个点，继续添加
+            self.nearest.append((current,d))
         else:
-            nearer_node = kd_node.right
-            further_node = kd_node.left
+            farthest_d = self._get_farthest()[1]
+            if farthest_d > d:
+                # 将旧的最远点移除
+                new_nearest = [item for item in self.nearest if item[1] < farthest_d]
+                # 添加新的点
+                new_nearest.append((current,d))
+                self.nearest = new_nearest
 
-        temp1 = travel(nearer_node,target,max_dist)
 
-        nearest = temp1.nearest_point #以此叶子节点为”当前最近点“
-        dist = temp1.nearest_dist #更新最近距离
 
-        nodes_visited += temp1.nodes_visited
+    def find_nearest(self,target,k=1):
+        tree = self.kd_tree.root
+        stack = []
+        self._search_leaf(stack,tree,target) #一直搜索直到叶子结点，并将路径压入栈
+        while stack:
+            node,next_direction = stack.pop() #弹出栈顶为当前最近点
+            dom_elt,axes,lchild,rchild = node.get_attr()
+            # 因为要添加k个临近点，所以需要检查当前点到目标点的距离
+            self._check_nearest(dom_elt,target,k)
+            if lchild and rchild is None: #当前结点为叶子结点，则回退
+                continue
+            #获取最远点距离，如果k=1，即获取当前最近点与目标点的距离
+            farthest,distance = self._get_farthest()
+            if abs(dom_elt[axes] - farthest[axes]) < distance: #判断是否与另一半区域相交
+                if next_direction == 'right':
+                    try_node = lchild
+                else:
+                    try_node = rchild
+                self._search_leaf(stack,try_node,target)
+        return self.nearest
 
-        if dist < max_dist:
-            max_dist = dist # 最近点在以目标点为球心，目标点到“当前最近点”的距离为半径的超球体内
-
-        temp_dist = abs(pivot[s] - target[s]) # 第s维上目标点与分割超平面的距离
-        if max_dist < temp_dist: #判断超球体是否与超平面 相交
-            return result(nearest,dist,nodes_visited) #不相交则直接返回
-
-        # 计算目标点与分割点的欧氏距离
-        temp_dist = sqrt(sum((p1-p2)**2 for p1,p2 in zip(pivot,target)))
-
-        if temp_dist < dist: #如果更近
-            nearest = pivot #更新最近点
-            dist = temp_dist #更新最近距离
-            max_dist = dist #更新超球体半径
-
-        # 检查另一个子结点对应的区域是否有更近的点
-        temp2 = travel(further_node,target,max_dist)
-
-        nodes_visited += temp2.nodes_visited
-        if temp2.nearest_dist < dist: #如果另一个子结点内存在更近的点
-            nearest = temp2.nearest_point
-            dist = temp2.nearest_dist
-
-        return result(nearest,dist,nodes_visited)
-
-    return travel(tree.root,point,float("inf"))
-
-#TODO：KD树进行分类
 
 if __name__ == "__main__":
-    start = time.time()
-    (train_images, train_labels), (test_images, test_labels) = load_data('../data/MNIST/')
-    knn = KNN(train_images,train_labels)
-    score = knn.score(test_images[:100],test_labels[:100])
-    end = time.time()
-    print(score)
-    print("time:{:.4f}s".format(end-start))
+    # start = time.time()
+    # (train_images, train_labels), (test_images, test_labels) = load_data('../data/MNIST/')
+    # knn = KNN(train_images,train_labels)
+    # score = knn.score(test_images[:100],test_labels[:100])
+    # end = time.time()
+    # print(score)
+    # print("time:{:.4f}s".format(end-start))
+
+    data = np.array([[6.27, 5.50],
+                     [1.24, -2.86],
+                     [17.05, -12.79],
+                     [-6.88, -5.40],
+                     [-2.96, -0.50],
+                     [7.75, -22.68],
+                     [10.80, -5.03],
+                     [-4.60, -10.55],
+                     [-4.96, 12.61],
+                     [1.75, 12.26],
+                     [15.31, -13.16],
+                     [7.83, 15.70],
+                     [14.63, -0.35]])
+
+    labels = np.array([1, 2, 3, 2, 2, 3, 3, 2, 1, 1, 3, 1, 3])
+
+    knn = KnnBasedOnKdTree(data,labels)
+    target = np.array([4,10])
+    nearest = knn.find_nearest(target,1)
+    print(nearest)
+    plt.figure(dpi=100)  # 指定像素
+    for i in range(len(labels)):
+        if labels[i] == 1:
+            color = 'c'
+        elif labels[i] == 2:
+            color = 'b'
+        else:
+            color = 'g'
+        plt.scatter(data[i][0], data[i][1], c=color, s=30, alpha=0.8)
+
+    plt.scatter(target[0], target[1], c='r', s=30, alpha=0.8)
+
+    for point,_ in nearest:
+        plt.plot([target[0], point[0]], [target[1], point[1]], 'r--')
+
+    plt.show()
 
 
 
