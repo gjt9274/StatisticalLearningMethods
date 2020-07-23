640f9acc8c50ea697e72ccc26166a597e164cf28
gjt9274 <gjt9274@gmail.com>
2020-07-23 23:05:26 +0800

    naive bayes

--- MultiClassification/naive_bayes.py
+++ MultiClassification/naive_bayes.py
@@ -27,6 +27,7 @@ class NaiveBayes:
     def _calculate_prob(self, x, y, lamda):
         """
         计算先验概率和条件概率
+        _____
         Args:
             x (array):  样本特征 (n, 28x28)
             y (array): 样本标签数组 (n, 1)
