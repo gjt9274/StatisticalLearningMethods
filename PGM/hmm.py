a1dff4c5d82fcda6b23f1c4a2800217d111ebbff
gjt9274 <gjt9274@gmail.com>
2020-07-27 17:02:39 +0800

    hidden markov

---
+++ PGM/hmm.py
@@ -0,0 +1,43 @@
+"""
+@File:    hmm
+@Author:  GongJintao
+@Create:  7/27/2020 11:08 AM
+@Software:Pycharm
+@Blog:    https://gongjintao.com
+@Email:   gjt9274@gmail.com
+"""
+
+import numpy as np
+import pandas as pd
+
+class HiddenMarkov:
+    def __init__(self):
+        pass
+
+    # 前向算法
+    def forward(self):
+        pass
+
+    # 后向算法
+    def backward(self):
+        pass
+
+    # 维特比算法
+    def viterbi(self):
+        pass
+
+    # 测试函数
+    def test(self):
+        pass
+
+    # F1分数
+    def _f1(self):
+        pass
+
+    # 召回率
+    def _recall(self):
+        pass
+
+    # 精确率
+    def _precision(self):
+        pass
